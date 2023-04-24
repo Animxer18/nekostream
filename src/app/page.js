ac641ab509054aa9e85e80f182aaf1c0cdf5a3ac
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-04-24 21:45:32 +0530

    page

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { API_URL } from "@/constant/constant";
 const inter = Inter({ subsets: ["latin"] });
 
 async function getTopAiring() {
-  const res = await fetch(API_URL + "top-airing");
+  const res = await fetch(API_URL + "top-airing", { cache: "no-store" });
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -14,6 +14,7 @@ async function getTopAiring() {
 
 export default async function Home() {
   const topAiring = await getTopAiring();
+  console.log("top anime", topAiring.results);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
