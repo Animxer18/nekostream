76d749f9aa6aed9868a112cbfb4639bd250f35c1
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-04-24 23:51:33 +0530

    graphql

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { API_URL } from "@/constant/constant";
+import { AL_API, API_URL } from "@/constant/constant";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +12,56 @@ async function getTopAiring() {
   return res.json();
 }
 
+// AniList Api Functions
+function handleResponse(response) {
+  return response.json().then(function (json) {
+    return response.ok ? json : Promise.reject(json);
+  });
+}
+
+function handleData(data) {
+  console.log("AL-data", data["data"]["Media"]);
+}
+
+function handleError(error) {
+  console.error(error);
+}
+
+async function getALData() {
+  var query = `query ($id: Int, $search: String) {
+  Media (id: $id, type:ANIME, search: $search) {
+    id
+    title {
+      romaji,
+      english
+    }
+    episodes,
+    type
+  }
+}`;
+
+  var variables = {
+    search: "to your eternity",
+  };
+  var url = AL_API,
+    options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: variables,
+      }),
+    };
+  fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
+}
+
 export default async function Home() {
   const topAiring = await getTopAiring();
-  console.log("top anime", topAiring.results);
-
+  const ALData = await getALData();
+  console.log("aaa", ALData);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Top Airing Anime</h1>
--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,4 +1,7 @@
 // cosumet API URL
 const API_URL = "https://api.consumet.org/anime/gogoanime/";
 
-export { API_URL };
+// AniList API URL
+const AL_API = "https://graphql.anilist.co";
+
+export { API_URL, AL_API };
