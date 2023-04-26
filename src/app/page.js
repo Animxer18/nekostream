19c4243cecf776afeb0866fd1427578f2672d763
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-04-26 23:44:59 +0530

    page

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ async function getTopAiring() {
 }
 
 // AniList Api Functions
+
 // function handleResponse(response) {
 //   return response.json().then(function (json) {
 //     return response.ok ? json : Promise.reject(json);
