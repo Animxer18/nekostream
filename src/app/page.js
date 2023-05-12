87a609f1b4273ed3b263aa756b30afa4e590b723
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2023-05-12 13:57:39 +0530

    src

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import { AL_API, GOGO_API_URL } from "@/constant/constant";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
 
 async function getTopAiring() {
   const res = await fetch(GOGO_API_URL + "top-airing", { cache: "no-store" });
@@ -109,46 +106,45 @@ export default async function Home() {
   console.log("top", topAiring);
   console.log("ALData", ALData);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24">
       <h1 className="text-2xl font-bold sm:text-3xl mb-5">Top Airing Anime</h1>
-      <div className="w-full flex flex-wrap gap-4 justify-center">
+      <div className="w-full flex flex-wrap justify-center">
         {topAiring.results.map((item, id) => {
           return (
-            <div
-              className="group relative block bg-black w-[90%] md:w-1/5"
-              key={id}
-            >
-              <Link href="/">
-                <Image
-                  alt="Developer"
-                  src={
-                    item.image
-                      ? item.image
-                      : "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx114535-y3NnjexcqKG1.jpg"
-                  }
-                  fill
-                  className="absolute inset-0 h-full w-full object-cover opacity-[0.5] transition-opacity group-hover:opacity-30"
-                />
-                <div className="relative p-4 sm:p-6 lg:p-8">
-                  <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
-                    # {id + 1}
-                  </p>
-                  <p className="text-xl font-bold text-white sm:text-2xl">
-                    {item.title ? item.title : item.id}
-                  </p>
-                  <div className="mt-32 sm:mt-48 lg:mt-64">
-                    <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-                      {item.genres.map((genre, key) => {
-                        return (
-                          <span key={key} className="text-sm text-white">
-                            {genre}{" "}
-                          </span>
-                        );
-                      })}
+            <div key={id} className="px-4 py-4 w-[90%] md:w-1/5">
+              <div className="group h-full relative block bg-black">
+                <Link href="/">
+                  <Image
+                    alt="Developer"
+                    src={
+                      item.image
+                        ? item.image
+                        : "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx114535-y3NnjexcqKG1.jpg"
+                    }
+                    fill
+                    className="absolute inset-0 h-full w-full object-cover opacity-[0.5] transition-opacity group-hover:opacity-20"
+                  />
+                  <div className="relative p-4 sm:p-6 lg:p-8">
+                    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
+                      # {id + 1}
+                    </p>
+                    <p className="text-xl font-bold text-white sm:text-2xl">
+                      {item.title ? item.title : item.id}
+                    </p>
+                    <div className="mt-32 sm:mt-48 lg:mt-64">
+                      <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                        {item.genres.map((genre, key) => {
+                          return (
+                            <span key={key} className="text-sm text-white">
+                              {genre}{" "}
+                            </span>
+                          );
+                        })}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Link>
+                </Link>
+              </div>
             </div>
           );
         })}
