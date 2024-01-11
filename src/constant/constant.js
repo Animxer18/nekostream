389e7906806e2a8f1fc2f94cafa1a9a5d5857a22
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2024-01-11 14:35:21 +0800

    Update constant.js

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -3,13 +3,13 @@ const Title = "nekostream";
 const Desc = "Watch anime online - Stream your favourite anime online for free";
 
 // gogoanime API URL
-const GOGO_API_URL = "https://api.consumet.org/anime/gogoanime/";
+const GOGO_API_URL = "https://animxer-api-cvxg.vercel.app/anime/gogoanime/";
 
 // Enime API URL
-const ENIME_API_URL = "https://api.consumet.org/anime/enime/";
+const ENIME_API_URL = "https://animxer-api-cvxg.vercel.app/anime/enime/";
 
 // zoro API URL
-const ZORO_API_URL = "https://api.consumet.org/anime/zoro/";
+const ZORO_API_URL = "https://animxer-api-cvxg.vercel.app/anime/zoro/";
 
 // AniList API URL
 const AL_API = "https://graphql.anilist.co";
